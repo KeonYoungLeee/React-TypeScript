3cdd7b8d6f774f2952985aebec63c90f0fecd2b2
KeonYoungLeee <ygy5036@gmail.com>
2020-08-31 18:37:19 +0900

    Add 강좌4(프로젝트 세팅)

---
+++ 4.  강좌4(틱택토)/clint.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { hot } from 'react-hot-loader/root';
+
+
+const Hot = hot();
+
+ReactDOM.render(<Hot />, document.querySelector('#root'));
