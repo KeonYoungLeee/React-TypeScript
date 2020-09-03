86935470d2f0eedc3874ca1ccf17195504551cf2
KeonYoungLeee <ygy5036@gmail.com>
2020-09-04 08:17:32 +0900

    Add 강좌6(프로젝트 세팅)

---
+++ 6.  강좌6(React-Router)/clint.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { hot } from 'react-hot-loader/root';
+
+import  from './MineSearch';
+
+const Hot = hot();
+
+ReactDOM.render(<Hot />, document.querySelector('#root'));
